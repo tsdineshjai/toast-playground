fb23ba6d9b7eff9870bedc27db5c3005bded968a
tanishka <tsdineshjai@gmail.com>
2024-03-09 07:32:01 +0530

    exercise one is done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import ToastPlayground from '../ToastPlayground';
-import Footer from '../Footer';
+import ToastPlayground from "../ToastPlayground";
+import Footer from "../Footer";
 
 function App() {
-  return (
-    <>
-      <ToastPlayground />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<ToastPlayground />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
